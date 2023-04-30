3d7f5f460b0dee0963b27d9fd1648646da8e12a9
Yusuf-R <y.abdulwasiu@gmail.com>
2023-04-30 01:45:54 +0100

    fix: :bug: added ensue attribute

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,10 @@
 # create a file using puppet
 
 file { 'school_file':
+  ensure  => 'present'
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
   content => 'I love Puppet',
-  path    => '/temp/school'
+  path    => '/temp/school',
 }
