2ce80443a9836bac57e465621d066eeb85e7cda6
Yusuf-R <y.abdulwasiu@gmail.com>
2023-04-30 01:37:16 +0100

    fix: :bug: ensure the file is present

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -4,9 +4,10 @@ $text = 'I love Puppet'
 $ow_grp = 'www-data'
 
 file { 'school_file':
+  ensure  => 'present',
   mode    => '0744',
   owner   => $ow_grp,
   group   => $ow_grp,
   content => $text,
-  path    => $path
+  path    => $path,
 }
