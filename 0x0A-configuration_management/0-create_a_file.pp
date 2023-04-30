d177dbf51c4860cfd1f4cff17ee5a44bc677c624
Yusuf-R <y.abdulwasiu@gmail.com>
2023-04-30 01:56:36 +0100

    fix: :bug: silence

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,13 +1,9 @@
 # create a file using puppet
-$name = '/tmp/school'
-$path = '/tmp'
-$text = 'I love Puppet'
-$ow_grp = 'www-data'
 
-file { $name:
+file { '/tmp/school':
   ensure  => 'present',
   mode    => '0744',
-  owner   => $ow_grp,
-  group   => $ow_grp,
+  owner   => 'www-data',
+  group   => 'www-data',
   content => $text,
 }
