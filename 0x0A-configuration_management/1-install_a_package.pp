3797426fb33fba7dfb189b22bf5953243b13c96f
Yusuf-R <y.abdulwasiu@gmail.com>
2023-04-30 03:03:14 +0100

    feat: :sparkles: istall flask via pip3

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,17 +1,6 @@
-# A Puppet manifest to installs `flask` from `pip3`
-
-exec { 'update-apt':
-    command     => '/usr/bin/apt-get update',
-    refreshonly => true,
-}
-
-package { 'python3-pip':
-    ensure  => installed,
-    require => Exec['update-apt'],
-}
+# install flask from pip3
 
 package { 'flask':
-    ensure   => '2.1.0',
-    provider => 'pip3',
-    require  => Package['python3-pip'],
+  ensure   => '2.1.0'
+  provider => pip3,
 }
