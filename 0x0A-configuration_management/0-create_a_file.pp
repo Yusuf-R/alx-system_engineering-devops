3a5b8d5dc34150d0c3dca8dc00c2b231e074a969
Yusuf-R <y.abdulwasiu@gmail.com>
2023-04-30 01:38:50 +0100

    fix: :bug: change the title

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -3,11 +3,9 @@ $path = '/temp/school'
 $text = 'I love Puppet'
 $ow_grp = 'www-data'
 
-file { 'school_file':
-  ensure  => 'present',
+file { $path:
   mode    => '0744',
   owner   => $ow_grp,
   group   => $ow_grp,
   content => $text,
-  path    => $path,
 }
