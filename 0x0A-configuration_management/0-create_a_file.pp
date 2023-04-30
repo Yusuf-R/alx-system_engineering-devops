1d6bc7466a4dcd20d87ba1585969ebcda447bce0
Yusuf-R <y.abdulwasiu@gmail.com>
2023-04-30 01:47:07 +0100

    fix: :bug: fixed the title

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,10 +1,9 @@
 # create a file using puppet
 
-file { 'school_file':
+file { '/temp/school':
   ensure  => 'present'
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
   content => 'I love Puppet',
-  path    => '/temp/school',
 }
