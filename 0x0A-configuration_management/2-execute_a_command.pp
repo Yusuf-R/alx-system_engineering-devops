794e9f60259463356aa5a5edcf2b1c3c78588728
Yusuf-R <y.abdulwasiu@gmail.com>
2023-04-30 03:52:50 +0100

    fix: :bug: added a path for pkill

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,5 +1,6 @@
 # kills a process named killmenow.
 
 exec { 'killmenow':
-  command => 'pkill -9 $(ps aux | grep "killmenow"',
-  }
+  command => 'pkill -9 $(ps aux | grep killmenow | awk \'{print $2}\')',
+  path => ['/usr/bin', '/sbin', '/bin'],
+}
