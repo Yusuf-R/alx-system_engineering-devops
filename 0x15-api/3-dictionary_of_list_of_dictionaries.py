680d20370d8f3f6fc960f8facbdc41d7c726eda1
Yusuf-R <y.abdulwasiu@gmail.com>
2023-07-25 16:33:35 +0100

    feat: :sparkles: records all tasks from all employees

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,43 +1,43 @@
 #!/usr/bin/python3
 """Module 0-gather_data_from_an_API.py"""
-import csv
-import requests
-import sys
+
+import json
+from requests import get
 
 
-def to_do(num):
-    """Returns the number of tasks for a given employee ID"""
-    if type(num) == int:
-        url_todo = "https://jsonplaceholder.typicode.com/todos"
-        url_user = "https://jsonplaceholder.typicode.com/users"
+def to_do():
+    """Records all tasks from all employees"""
+    all_usr = "https://jsonplaceholder.typicode.com/users"
 
-        raw_todo = requests.get(url_todo).json()
-        raw_user = requests.get(url_user).json()
+    dummy = {}
+    all_usr_pyt_obj = {}
+    ls_dict = []
+    file = "todo_all_employees.json"
 
-        emp_name = ""
-        emp_id = 0
-        py_form = []
-        for j in raw_user:
-            if j["id"] == num:
-                emp_name = j["username"]
-        for i in raw_todo:
-            if i["userId"] == num:
-                emp_id = i["userId"]
-                py_form.append("{}, {}, {}, {}".
-                               format(i["userId"],
-                                      emp_name,
-                                      i["completed"],
-                                      i["title"]))
-        file = "{}.csv".format(emp_id)
-        with open(file, mode="w", newline="") as f:
-            wr = csv.writer(f, quoting=csv.QUOTE_ALL)
-            for k in py_form:
-                csv_form = [val.strip() for val in k.split(", ")]
-                wr.writerow(csv_form)
+    all_user_data = get(all_usr).json()
 
-    else:
-        return
+    for user in all_user_data:
+        username = user["username"]
+        id = user["id"]
+
+        uid_todo = ("https://jsonplaceholder.typicode.com/users/{}/todos"
+                    .format(id))
+        uid_todo_data = get(uid_todo).json()
+
+        for i in uid_todo_data:
+            dummy["username"] = username
+            dummy["task"] = i["title"]
+            dummy["completed"] = i["completed"]
+            ls_dict.append(dummy)
+            dummy = {}
+
+        all_usr_pyt_obj[str(id)] = ls_dict
+        ls_dict = []
+
+    with open(file, mode="w", newline="") as f:
+        json.dump(all_usr_pyt_obj, f, indent=2)
 
 
 if __name__ == "__main__":
-    to_do(int(sys.argv[1]))
+    """Lauch the script"""
+    to_do()
