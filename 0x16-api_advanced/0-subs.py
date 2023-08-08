f73bec47ed8b20cbb15af1267583e7319f62c40a
Yusuf-R <y.abdulwasiu@gmail.com>
2023-08-08 21:49:09 +0100

    fix: :bug: set redirect to false

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -13,7 +13,7 @@ def number_of_subscribers(subreddit):
 
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {"User-Agent": "Mozilla/5.0"}
-    req = requests.get(url, headers=headers)
+    req = requests.get(url, headers=headers, allow_redirects=False)
     if req.status_code != 200:
         return 0
     return req.json().get("data").get("subscribers")
