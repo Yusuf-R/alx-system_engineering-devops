45156422f4a064efec84c012a31f1445ac3567d9
Yusuf-R <y.abdulwasiu@gmail.com>
2023-08-08 21:45:45 +0100

    feat: :sparkles: queries the Reddit API and returns the number of subscribers

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+This module queries the Reddit API and returns the number of subscribers
+"""
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """
+    This function queries the Reddit API and returns the number of subscribers
+    """
+
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "Mozilla/5.0"}
+    req = requests.get(url, headers=headers)
+    if req.status_code != 200:
+        return 0
+    return req.json().get("data").get("subscribers")
