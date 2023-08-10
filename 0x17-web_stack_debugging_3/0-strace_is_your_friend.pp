d54adb8fcd79cd08278a153d57d7c4a60eb2faf4
Yusuf-R <y.abdulwasiu@gmail.com>
2023-08-10 03:23:15 +0100

    fix: :bug: fixed the double slash to single

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 # Using strace to resolve the internal server Error (500)
 exec { 'fixed-phpp':
-        command  => 'sed -i "s//class-wp-locale.phpp/class-wp-locale.php/g /var/www/html/wp-settings.php"',
+        command  => 'sed -i "s/class-wp-locale.phpp/class-wp-locale.php/g /var/www/html/wp-settings.php"',
         path     => '/bin',
         provider => 'shell',
 }
