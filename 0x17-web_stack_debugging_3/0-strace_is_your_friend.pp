ec5b21ec296a2b93c4fb01ccc0c4e93126c6dd5c
Yusuf-R <y.abdulwasiu@gmail.com>
2023-08-10 03:02:13 +0100

    feat: :sparkles: using strace, find out why Apache is returning a 500 error

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,7 @@
+# Using strace to resolve the internal server Error (500)
+exec { 'fixed-phpp':
+	command => 'sed -i "s//class-wp-locale.phpp/class-wp-locale.php/g /var/www/html/wp-settings.php"',
+	path    => '/bin',
+	provider => 'shell',
+}
+
---
+++ 0x17-web_stack_debugging_3/README.md
@@ -0,0 +1,4 @@
+#0x17. Web stack debugging #3
+
+## using strace to trace the error 
+## uesed sed for inline editing of the error
