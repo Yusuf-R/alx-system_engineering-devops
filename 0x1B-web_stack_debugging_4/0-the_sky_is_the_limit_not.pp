037536747d4e6627c7215653a1fab8471ed7b77d
Yusuf <y.abdulwasiu@gmail.com>
2023-08-17 15:40:51 +0100

    refactored to 4096

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,6 @@
 # Increase the base limit of demon process to handle high volume request
 exec { 'Incrase-nginx-daemon-limit':
-        command  => "sed -i 's/LIMIT=\"-n 15\"/LIMIT=\"-n 9999\"/g' /etc/default/nginx",
+        command  => "sed -i 's/LIMIT=\"-n 15\"/LIMIT=\"-n 4096\"/g' /etc/default/nginx",
         path     => '/bin',
         provider => 'shell'
 }
