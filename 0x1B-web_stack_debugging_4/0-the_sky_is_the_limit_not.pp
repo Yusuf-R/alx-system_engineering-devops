25f9e550ae74fec8d5642bff8eeacebe01c7b88c
Yusuf-R <y.abdulwasiu@gmail.com>
2023-08-17 15:39:02 +0100

    feat: :sparkles: increase the limit of request for nginx server

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,11 @@
+# Increase the base limit of demon process to handle high volume request
+exec { 'Incrase-nginx-daemon-limit':
+        command  => "sed -i 's/LIMIT=\"-n 15\"/LIMIT=\"-n 9999\"/g' /etc/default/nginx",
+        path     => '/bin',
+        provider => 'shell'
+}
+# Restart Nginx service
+-> exec { 'Restart Nginx':
+        command => 'sudo nginx restart',
+        path    => '/etc/init.d/',
+}
